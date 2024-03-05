a403c8a95ed0f92a5cbd243f8446e2ba9f2bf76b
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2024-03-05 20:11:10 +0530

    Custom Bootstrap theme and logo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
