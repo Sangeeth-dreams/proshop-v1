37a6693173db26c5fabc89042d40f7f79c1b407d
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2024-03-08 11:26:36 +0530

    Get products from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+const router = express.Router();
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      return res.send(product);
+    }
+
+    res.status(404).json({ message: "Product not found" });
+  })
+);
+
+export default router;
