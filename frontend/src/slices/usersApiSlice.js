63b19bff5e5d44edc54298ca06ecdebddddabc5b
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2024-03-11 22:29:06 +0530

    Login Functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -5,7 +5,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: USERS_URL / auth,
+        url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
       }),
@@ -13,4 +13,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useLoginMutation } = userssApiSlice;
+export const { useLoginMutation } = usersApiSlice;
