3c2f3ecaf914319f88ef18e180a6aa59e0142b7c
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2024-03-11 10:21:52 +0530

    Auth Middleware and Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -10,9 +11,13 @@ const port = process.env.PORT || 5000;
 connectDB(); //connect to MongoDB
 
 const app = express();
+
 // Bady parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
