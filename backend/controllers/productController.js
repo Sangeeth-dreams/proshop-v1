3808c80b9048a19fa5a66495f7934739bc8bcee5
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2024-06-29 23:14:13 +0530

    Creating Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,24 @@ const getProductsById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductsById };
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductsById, createProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   getProducts,
   getProductsById,
+  createProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductsById);
 
 export default router;
